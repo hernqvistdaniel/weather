130a70b11d9cb899e3882182fdd0e51f39dd56df
Daniel Hernqvist <daniel.hernqvist@chasacademy.se>
2019-03-17 14:30:12 +0100

    Updates

---
+++ precache-manifest.e0d3a35cff9c31cdf5f79a450c34ff28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8e431487211a2969723",
+    "url": "/weather/static/css/main.84b40b43.chunk.css"
+  },
+  {
+    "revision": "d8e431487211a2969723",
+    "url": "/weather/static/js/main.00fe2806.chunk.js"
+  },
+  {
+    "revision": "3cb3cde5c978189545b4",
+    "url": "/weather/static/js/runtime~main.6cff1e35.js"
+  },
+  {
+    "revision": "45da66964be97731cdab",
+    "url": "/weather/static/css/2.cb891a02.chunk.css"
+  },
+  {
+    "revision": "45da66964be97731cdab",
+    "url": "/weather/static/js/2.760b29d1.chunk.js"
+  },
+  {
+    "revision": "105db0abca29fe7a70a349b92a993202",
+    "url": "/weather/index.html"
+  }
+];
